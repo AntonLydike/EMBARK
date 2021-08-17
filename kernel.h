39a5eada2f19957b710eb78920277b16ea4befb3
Anton Lydike <me@antonlydike.de>
2021-08-17 09:14:45 +0200

    introduced data structure to manage binaries in memory

--- kernel.h
+++ kernel.h
@@ -5,18 +5,28 @@
 #define false 0
 
 #define XLEN          32            // 32 bit system
-#define MUTEX_COUNT   64            // must be multiple of xlen
-#define PROCESS_COUNT 64
-#define MAX_INT       0x7FFFFFFF    // max 32 bit signed int
-
-// memory layout:
-#define ROM_START 0x00100
-#define IO_START  0x10000
-#define NVM_START 0x20000
-#define RAM_START 0x50000
+#define PROCESS_COUNT 32            // number of concurrent processes
+#define NUM_BINARIES  16            // number of binaries loaded simultaneously 
 
 // scheduler settings
 #define TIME_SLICE_LEN 100          // number of cpu time ticks per slice
+
+
+/* This struct holds information about binaries which are currently loaded into
+ * memory. Currently the kernel is not able to load binaries into memory, as
+ * no file system layer is implemented. When the memory image is built, the 
+ * list of loaded binaries is populated aswell.
+ */
+typedef struct loaded_binary {
+    int binid;
+    int entrypoint;
+    int bounds[2];
+} loaded_binary;
+
+// create a global table holding all loaded binaries.
+// this is either populated at runtime when binaries are loaded dynamically
+// or when a memory image is created.
+extern loaded_binary binary_table[NUM_BINARIES];
 
 // init function
 extern __attribute__((__noreturn__)) void init();
