cc498154aaa2548b17ac32988bfd7a41b26b4952
Anton Lydike <me@antonlydike.de>
2021-08-18 13:06:16 +0200

    moved binary_table from .bss to .data section to prevent overwriting it with zeros on startup

--- kernel.c
+++ kernel.c
@@ -6,7 +6,7 @@ void create_processes_from_bin_table();
 
 extern ProcessControlBlock processes[PROCESS_COUNT];
 
-loaded_binary binary_table[NUM_BINARIES];
+loaded_binary binary_table[NUM_BINARIES] __attribute__ ((section (".data")));
 
 extern void init()
 {
