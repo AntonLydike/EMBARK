f2e83a61ac802770255a0be98cdc63af4625fe84
Anton Lydike <me@antonlydike.de>
2021-08-22 13:19:49 +0200

    added a debugging constant to identify absence of textIO

--- kinclude/io.h
+++ kinclude/io.h
@@ -4,6 +4,9 @@
 // if we have a textIO module
 #ifdef TEXT_IO_ADDR
 
+#define DEBUGGING 1
+
+
 #ifndef TEXT_IO_BUFLEN
 #error "When defining TEXT_IO_ADDR, please also provide TEXT_IO_BUFLEN, otherwise textIO won't work!"
 #endif
@@ -14,7 +17,11 @@ void dbgln(char* text, int len);
 /* alphabet for itoa */
 char* itoa (int value, char* str, int base);
 
+// if we don't have a textIO module for debugging
 #else
+
+#define DEBUGGING 0
+
 
 // if we don't have textio, dbgln becomes an empty macro to save on cycles
 #define dbgln(a,b)
@@ -22,5 +29,4 @@ char* itoa (int value, char* str, int base);
 #define itoa(a,b,c) b
 
 #endif
-
 #endif
